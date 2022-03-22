776f71e797365c6fe20ec013c3f983eeb7b46c72
mkoo-moncel <martin.koo@moncel.com>
2022-03-21 21:53:35 -0400

    generic Head and add fonts

---
+++ apps/icx-next/components/Head/index.tsx
@@ -0,0 +1,15 @@
+
+// @ts-nocheck
+import Head from 'next/head';
+
+const _Head = () => {
+    return <Head>
+        <title>Food Handler Certificate Course Online | 100% Online</title>
+        <meta name="description" content="ICX website" />
+        {/* bootstrap responsive viewport meta tag */}
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1" />  */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+};
+
+export default _Head;
