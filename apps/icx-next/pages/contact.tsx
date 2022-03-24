7e0bf0b641013c1486644cb816a5bf86812c72f9
mkoo-moncel <martin.koo@moncel.com>
2022-03-24 15:49:57 -0400

    contact form

--- apps/icx-next/pages/contact.tsx
+++ apps/icx-next/pages/contact.tsx
@@ -18,13 +18,35 @@ const Contact = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
                 <h1>Contact Us</h1>
             </div>
         </div>
-        <div className={contactStyles['contact-form']}>
-            <div className={styles.line} />
-            <h2>General Enquiry</h2>
-            <form id="form-enquiry" name="form-general-icc" action="https://formspree.io/f/xoqyzdzy" method="POST" className="clearfix form-validation"> 
+        <div className={`${contactStyles['contact-form']} ${styles['three-col-section']}`}>
+            <div className={styles.center}>
+                <div className={contactStyles.title}>
+                    <div className={styles.line} />
+                    <h2>General Enquiry</h2>
+                </div>
+                <form id="form-enquiry" name="form-general-icc" action="https://formspree.io/f/xoqyzdzy" method="POST" className="clearfix form-validation"> 
+                    <p>If you have a general enquiry, please fill out the form below.</p>
+                    <div className="row">
+                        <div className="form-group col-6"> <label htmlFor="firstname">First Name *</label> <input required name="firstname" type="text" className="form-control" id="form-enquiry-firstname" aria-describedby="" placeholder="John" /> <div className="feedback invalid-feedback">Please provide your first name</div> </div> <div className="form-group col-6"> <label htmlFor="lastname">Last Name *</label> <input required name="lastname" type="text" className="form-control" id="form-enquiry-lastname" aria-describedby="" placeholder="Smith" /> <div className="feedback invalid-feedback">Please provide your last name</div></div>
+                    </div>
 
-            </form>
+                    <div className="row">
+                        <div className="form-group col-6"> <label htmlFor="email">Email Address *</label> <input required name="email" type="email" className="form-control" id="form-enquiry-email" aria-describedby="" placeholder="e.g. john@email.com" /> <div className="feedback invalid-feedback">Please provide a valid email address</div> </div> <div className="form-group col-6"> <label htmlFor="phone">Phone Number *</label> <input type="text" name="phone" className="form-control" id="form-enquiry-phone" aria-describedby="" placeholder="e.g. 1 400-000-0000" /> <div className="feedback invalid-feedback">Please provide your phone number</div> </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="form-group col-12"> <label htmlFor="subject">Subject *</label> <input required name="subject" type="text" className="form-control" id="form-enquiry-subject" aria-describedby="" placeholder="In just a few words, describe your enquiry" /> <div className="feedback invalid-feedback">Please provide a subject line</div> </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="form-group col-12"> <label htmlFor="message">Message *</label> <textarea required name="message" className="form-control" id="form-enquiry-message" aria-describedby="" rows={10} cols={50} placeholder="Provide a detailed description of your enquiry"></textarea> <div className="feedback">Please provide a detailed description of your enquiry</div> </div>
+                    </div>
+
+                    <button id="form-enquiry-submit" type="submit" className="mt-4 btn btn-lg btn-cta float-left"> <span className="d-none d-sm-block">Submit Enquiry</span> <span className="d-sm-none">Submit</span> </button>
+                </form>
+            </div>
         </div>
+        <Footer footer={footer} />
     </div>
 };
 
