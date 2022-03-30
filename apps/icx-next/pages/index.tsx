178d0908f3df2f5a53ecb15350442839888ce07f
endlessorhub <anthonyhutchi99@gmail.com>
2022-03-30 09:16:21 -0400

    Implement Tailwind

--- apps/icx-next/pages/index.tsx
+++ apps/icx-next/pages/index.tsx
@@ -30,7 +30,9 @@ const Home = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
         <div className={icxstyles['full-row']}>
           <HeroSection />
         </div>
-
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <div className={icxstyles.center}>
           <StepsSection />
         </div>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./apps/icx-next/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
