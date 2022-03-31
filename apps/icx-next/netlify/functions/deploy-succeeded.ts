c821f13ad488c2ba1223431b1ca25177966e5f78
mkoo-moncel <martin.koo@moncel.com>
2022-03-31 10:41:42 -0400

    test axios for snapshot build

--- apps/icx-next/netlify/functions/deploy-succeeded.ts
+++ apps/icx-next/netlify/functions/deploy-succeeded.ts
@@ -1,20 +1,17 @@
 import { Handler } from "@netlify/functions";
+import { executionAsyncResource } from "async_hooks";
+import axios from 'axios';
 
+const GITHUB_ACTION_EVENT_TYPE = "netlify_deploy_succeeded";
 const repoDispatchUrl = "https://api.github.com/repos/moncel/moncel-frontend/dispatches";
+const headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": "Basic bWtvby1tb25jZWw6Z2hwX2lBb3lwN0lxUDBzMU8zOU9rSzlWaE9mdUpjNFdUcTMwcTViaQ==",
+};
+const body = {"event_type": GITHUB_ACTION_EVENT_TYPE};
 
 const handler: Handler = async () => {
-  // Dispatch a 'deploy succeeded' event to github actions
-  const response = await fetch(repoDispatchUrl, {
-    method: "POST",
-    headers: {
-      "Accept": "application/vnd.github.v3+json",
-      "Authorization": "Basic bWtvby1tb25jZWw6Z2hwX2lBb3lwN0lxUDBzMU8zOU9rSzlWaE9mdUpjNFdUcTMwcTViaQ==",
-    },
-    body: JSON.stringify({
-      "event_type": "netlify_deploy_succeeded",
-    })
-  });
-
+  const response = await axios.post(repoDispatchUrl, body, { headers });
   return {
     statusCode: 200,
     body: JSON.stringify({ response }),
