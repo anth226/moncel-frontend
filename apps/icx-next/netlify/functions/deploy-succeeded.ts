0dde8b8b74661b4e253c092c80754804a66d8cfe
mkoo-moncel <martin.koo@moncel.com>
2022-04-01 06:22:30 -0400

    test netlify function secrets

--- apps/icx-next/netlify/functions/deploy-succeeded.ts
+++ apps/icx-next/netlify/functions/deploy-succeeded.ts
@@ -1,20 +1,22 @@
 import { Handler } from "@netlify/functions";
-import { executionAsyncResource } from "async_hooks";
 import axios from 'axios';
 
 const GITHUB_ACTION_EVENT_TYPE = "netlify_deploy_succeeded";
 const repoDispatchUrl = "https://api.github.com/repos/moncel/moncel-frontend/dispatches";
+
+if(process.env.GH_REPO_ACCESS_HEADER === undefined) throw Error("Environment variable GH_REPO_ACCESS_HEADER not defined; function will not be able to see private repo");
 const headers = {
     "Accept": "application/vnd.github.v3+json",
-    "Authorization": "Basic bWtvby1tb25jZWw6Z2hwX2lBb3lwN0lxUDBzMU8zOU9rSzlWaE9mdUpjNFdUcTMwcTViaQ==",
+    "Authorization": process.env.GH_REPO_ACCESS_HEADER || "",
 };
 const body = {"event_type": GITHUB_ACTION_EVENT_TYPE};
 
 const handler: Handler = async () => {
   const response = await axios.post(repoDispatchUrl, body, { headers });
+  console.log(response.status, response.statusText);
+  
   return {
     statusCode: 200,
-    body: JSON.stringify({ response }),
   };
 };
 
