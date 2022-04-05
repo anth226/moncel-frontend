53ed7aecf84cb660cd12ceb137209d3dc3408eac
mkoo-moncel <martin.koo@moncel.com>
2022-04-05 10:32:56 -0400

    fix styling errors on merge

--- apps/icx-next/components/certificate/benefits/index.tsx
+++ apps/icx-next/components/certificate/benefits/index.tsx
@@ -1,14 +1,13 @@
 import Image from 'next/image';
 
 import { StoryBlokCertificateBenefitContent, StoryBlokCard } from 'moncel-one-sdk/cms/types';
-import styles from './styles.module.scss';
 
 const Benefits = ({ benefits }: { benefits: StoryBlokCertificateBenefitContent}) => {
     const { benefits: cards } = benefits;
 
-    return <div className={styles.section}>
+    return <div className="row ps-6 pe-4 text-center text-md-start">
         {cards.map((card: StoryBlokCard, i) => {
-            return <div className="row ps-6 pe-4 text-center text-md-start" key={`benefits-${i}`}>
+            return <div className="col-12 col-md-6 mb-5" key={`benefits-${i}`}>
                 <Image src={card.icon.image} width={55} height={50} layout="fixed" alt={`benefits-${i}-icon`}/>
                 <h5 className="mt-3">{card.title}</h5>
                 <p className="mb-0">{card.description}</p>
