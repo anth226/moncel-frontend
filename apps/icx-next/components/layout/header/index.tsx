333703344d57cc8511ba507b0810a386ae430101
endlessorhub <anthonyhutchi99@gmail.com>
2022-04-06 17:25:17 -0400

    Create mobile menu, update sidebar
    https://moncel.atlassian.net/browse/DEV-122
    https://moncel.atlassian.net/browse/DEV-117

--- apps/icx-next/components/layout/header/index.tsx
+++ apps/icx-next/components/layout/header/index.tsx
@@ -1,40 +1,31 @@
-import { useState } from 'react';
-import { useMediaQuery } from 'react-responsive'
 import Image from 'next/image';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import Link from 'next/link';
 
 import { StoryBlokHeader } from 'moncel-one-sdk/cms/types';
 
 const Header = ({ header }: { header: StoryBlokHeader }) => {
-    const [expanded] = useState(false);
-    const isMobile = useMediaQuery({
-        query: '(max-width: 768px)'
-    });
+
 
     return <div className="bg-dark">
-        <nav className="container py-4">
-            <div className="d-flex justify-content-between align-items-center">
-                <div>
-                    <Link href="/" passHref><a><Image src={header.logo.image} alt="icx-logo" width={160} height={30} /></a></Link>
-                </div>
-                {/* Desktop links */}
-                {isMobile ? null : <div>
+        <Navbar expand="md" className="m-0 p-0">
+            <Container className="py-3">
+                <Navbar.Brand href="/" className="d-flex align-items-center">
+                    <Image src={header.logo.image} alt="icx-logo" width={160} height={30} />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" className="m-0 p-0"><FontAwesomeIcon className="mobile-menu" icon={faBars} /></Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto d-flex justify-content-between align-items-center">
                     {header.navigation.map(link => {
-                        return <a className={`text-white ms-4 ${link.class}`} key={`header-link-${link.label}}`} href={link.url}>{link.label.toUpperCase()}</a>
+                        return <a className={`text-white ms-0 ms-md-3 ms-lg-4 mt-2 mt-md-0 ${link.class}`} key={`header-link-${link.label}}`} href={link.url}>{link.label.toUpperCase()}</a>
                     })}
-                </div>
-                }
-
-                {/* Mobile dropdown */}
-                {isMobile ?
-                    (<button className="navbar-toggler px-0 py-0" aria-expanded={expanded} aria-label="Toggle header navigation">
-                        <FontAwesomeIcon className="mobile-menu" icon={faBars} />
-                    </button>)
-                    : null}
-            </div>
-        </nav>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     </div>
 
 };
