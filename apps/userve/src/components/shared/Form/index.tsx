16053731061251ef08efeab6c764b20840530a10
endlessorhub <anthonyhutchi99@gmail.com>
2022-04-20 15:34:55 -0400

    Update business form css

---
+++ apps/userve/src/components/shared/Form/index.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from "react";
+
+const HubspotContactForm = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src='https://js.hsforms.net/forms/v2.js';
+        document.body.appendChild(script);
+        console.log('loading this');
+        script.addEventListener('load', () => {
+            // @TS-ignore
+            if (window.hbspt) {
+                // @TS-ignore
+                window.hbspt.forms.create({
+                    region: "na1",
+                    portalId: "21498581",
+                    formId: "9b2da8dd-d68c-4b6b-8ef5-820dde604edc",
+                    target: '#hubspotForm'
+                })
+            }
+        });
+    }, []);
+
+    return (
+        <div>
+            <div id="hubspotForm" className="p-8 rounded bg-white drop-shadow-md"></div>
+        </div>
+    );
+
+}
+
+export default HubspotContactForm;
--- apps/userve/src/components/shared/product.tsx
+++ apps/userve/src/components/shared/product.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import ReactMarkdown from 'react-markdown';
+import HubspotContactForm from 'src/components/shared/Form';
 
 import { Section } from 'src/components/core/Section';
 import { ProductFormStoryblok } from 'src/storyblok-component-types';
 
+
 const ProductFormSection = (props: ProductFormStoryblok) => {
     { console.log(props) }
-    return <div className="bg-cover bg-[url('https://www.userve.com/hubfs/web-assets/usx_business_bg.jpg')]">
+    return <div id="buslmform" className="bg-cover bg-[url('https://www.userve.com/hubfs/web-assets/usx_business_bg.jpg')]">
         <Section className="grid grid-cols-12">
             <div className="order-first col-span-6">
                 <h2 className="text-3xl font-bold text-white mb-6">{props.title}</h2>
@@ -15,10 +17,8 @@ const ProductFormSection = (props: ProductFormStoryblok) => {
             </div>
 
             <div className="order-last col-span-5 col-end-13">
-                <div dangerouslySetInnerHTML={{ __html: "<iframe src='https://share.hsforms.com/1my2o3daMS2uO9YIN3mBO3Acsset' scrolling='no' style='width:100%;height:1000px;overflow: hidden;'/>" }} />
+                <HubspotContactForm/>
             </div>
-
-
         </Section>
     </div>
 }
