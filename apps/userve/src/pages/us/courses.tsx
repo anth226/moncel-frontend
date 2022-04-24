69b431bd61ee3a946aafe55b6368ede45617861e
mkoo-moncel <martin.koo@moncel.com>
2022-04-24 05:39:11 -0400

    state selector no longer uses dummy data

--- apps/userve/src/pages/us/courses.tsx
+++ apps/userve/src/pages/us/courses.tsx
@@ -1,14 +1,12 @@
 import * as React from "react"
 import { graphql, PageProps } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from 'src/components/layout';
 import Head from 'src/components/head';
-import { Section, SectionFullWidth } from 'src/components/core/Section';
 import { AboutUsSection } from 'src/components/sections/landing';
 import { HeroSection, CoursesSection } from 'src/components/sections/courses'
 import { DataProps } from 'src/lib/storyblokSourceTypes';
-import Courses from "src/components/sections/courses/courses";
+import { CoursePageStoryblok } from 'src/storyblok-component-types';
 
 const pageStyles = {
   color: "#232129",
@@ -18,16 +16,23 @@ const pageStyles = {
 
 enum SLUGS {
   aboutUs="landing/about-us",
+  coursePages="courses/course-pages"
 }
 
 export default ({ data }: PageProps<DataProps>) => {
   const landingSlugs = data.landing?.nodes || [];
+  const courseSlugs = data.allStoryblokEntry?.nodes || [];
   const aboutUsSlug = landingSlugs.filter(slug => slug.full_slug === SLUGS.aboutUs).shift();
+  const coursePageStories = courseSlugs.filter(slug => slug.full_slug.match(SLUGS.coursePages));
 
   // parsing "" into JSON will error out/fail builds.
   // this is intentional and indicates an error retrieving data from storyblok
   const aboutUsContent = JSON.parse(aboutUsSlug?.content || "");
+  const coursePageContent: CoursePageStoryblok[] = coursePageStories.map(story => {
+    return JSON.parse(story.content || "") as CoursePageStoryblok;
+  })
   const seoContent = data.seo.nodes[0];
+
   return <div>
     <Head seo={seoContent} />
     <Layout>
