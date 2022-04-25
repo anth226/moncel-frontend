c08a605ee89a508da6880d3222598684eaba9dd1
endlessorhub <anthonyhutchi99@gmail.com>
2022-04-25 16:40:24 -0400

    Add faqs to courses, update title rules

--- apps/userve/src/components/coursePages/courseInfo.tsx
+++ apps/userve/src/components/coursePages/courseInfo.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
-import { Header5, Header2, Text } from 'src/components/shared/typography';
+import { Header2, Text } from 'src/components/shared/typography';
 import { Section, SectionFullWidth } from 'src/components/core/Section';
 import { CoursePageInfoSectionStoryblok } from 'src/storyblok-component-types';
-
 
 export default (props: CoursePageInfoSectionStoryblok) => {
 
     const infoCards = props.info || [];
-    { console.log(props) }
-    return <SectionFullWidth className="bg-lilac">
+    return <SectionFullWidth>
         <Section className="grid grid-cols-12">
             <div className="col-span-3">
-
+                {props.layout == "title-left" ? <><Header2>{props.title}</Header2><ReactMarkdown>{props.desc}</ReactMarkdown></> : <img src={props.image?.filename} alt={props.title}/>}                
             </div>
             <div className="col-span-8 col-end-13">
-                <Header2>{props.title || ""}</Header2>
-                <Text>{props.desc || ""}</Text>
+                <div className="mb-6">
+                    {props.layout == "title-right" ? <div><Header2>{props.title}</Header2><ReactMarkdown>{props.desc}</ReactMarkdown></div> : ""}
+                </div>
                 <div className="accordion">
                     {infoCards.map((card, i) => {
-                        { console.log(card) }
                         return <div className="accordion-item border-solid border-1 p-6 border-slate-300 bg-white rounded-xl">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 {card.Title || ""}
