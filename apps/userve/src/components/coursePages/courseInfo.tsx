8f736420f03a5d3d29abbca5e82dc3cae1c58ed6
endlessorhub <anthonyhutchi99@gmail.com>
2022-04-25 16:48:13 -0400

    Update faq css

--- apps/userve/src/components/coursePages/courseInfo.tsx
+++ apps/userve/src/components/coursePages/courseInfo.tsx
@@ -14,17 +14,17 @@ export default (props: CoursePageInfoSectionStoryblok) => {
                 {props.layout == "title-left" ? <><Header2>{props.title}</Header2><ReactMarkdown>{props.desc}</ReactMarkdown></> : <img src={props.image?.filename} alt={props.title}/>}                
             </div>
             <div className="col-span-8 col-end-13">
-                <div className="mb-6">
-                    {props.layout == "title-right" ? <div><Header2>{props.title}</Header2><ReactMarkdown>{props.desc}</ReactMarkdown></div> : ""}
+                <div>
+                    {props.layout == "title-right" ? <div className="mb-6"><Header2>{props.title}</Header2><ReactMarkdown>{props.desc}</ReactMarkdown></div> : ""}
                 </div>
                 <div className="accordion">
                     {infoCards.map((card, i) => {
-                        return <div className="accordion-item border-solid border-1 p-6 border-slate-300 bg-white rounded-xl">
+                        return <div className="accordion-item border-solid border-1 p-6 border-slate-300 bg-white rounded-xl mb-6 last:mb-0">
                             <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 {card.Title || ""}
                             </button>
                             <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                                <div class="accordion-body py-4 px-5">
+                                <div class="accordion-body">
                                     <Text className="!mb-0">{card.Description || ""}</Text>
                                 </div>
                             </div>
