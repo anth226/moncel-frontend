1f85fd25349638eb46587ff605cb764eaeda8fbb
endlessorhub <anthonyhutchi99@gmail.com>
2022-04-27 16:32:34 -0400

    Add links to cta box, update css

--- apps/userve/src/components/sections/landing/about-us.tsx
+++ apps/userve/src/components/sections/landing/about-us.tsx
@@ -6,17 +6,18 @@ import { AboutUsStoryblok } from 'src/storyblok-component-types';
 import { Header2, Text } from 'src/components/shared/typography';
 
 const AboutUsSection = (props: AboutUsStoryblok) => {
+    console.log(props)
     return <Section>
         <div className="bg-[url('../images/usx-cta-background.svg')] bg-[#72809d] bg-cover	rounded-2xl grid grid-cols-12 grid-flow-row drop-shadow-lg">
             <div className="col-span-12 md:col-span-6 text-white flex flex-col justify-center p-10 md:p-14">
-                <Header2><span className="text-white">{props.header}</span></Header2>
-                <Text>{props.subheader}</Text>
-                <ButtonTransparent className="mt-4 w-fit">
-                    <Link to="/us/about/about-us">{props.link_text}</Link>
-                </ButtonTransparent>
+                <Header2 className="!text-white">{props.header}</Header2>
+                <Text className="!mb-0">{props.subheader}</Text>
+                <Link to={props.url.url}>
+                    <ButtonTransparent>{props.link_text}</ButtonTransparent>
+                </Link>
             </div>
             <div className="md:col-span-6 hidden justify-center relative md:flex self-end">
-                <img src={props.image?.filename || ""} alt="About Userve" className="block absolute max-w-[400px] bottom-0"/>
+                <img src={props.image?.filename || ""} alt="About Userve" className="block absolute max-w-[400px] bottom-0" />
             </div>
         </div>
     </Section>;
