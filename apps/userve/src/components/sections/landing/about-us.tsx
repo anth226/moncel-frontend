8fbe549bc5bac47c2b868c79c1fa45baf82cc0fb
endlessorhub <anthonyhutchi99@gmail.com>
2022-04-28 10:22:51 -0400

    Change cta url to string

--- apps/userve/src/components/sections/landing/about-us.tsx
+++ apps/userve/src/components/sections/landing/about-us.tsx
@@ -11,7 +11,7 @@ const AboutUsSection = (props: AboutUsStoryblok) => {
             <div className="col-span-12 md:col-span-6 text-white flex flex-col justify-center p-10 md:p-14">
                 <Header2 className="!text-white">{props.header}</Header2>
                 <Text className="!mb-0">{props.subheader}</Text>
-                <a href={props.url.url}>
+                <a href={props.url}>
                     <ButtonTransparent>{props.link_text}</ButtonTransparent>
                 </a>
             </div>
