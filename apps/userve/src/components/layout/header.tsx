4079547ef774806863a6558eb263c43d248a89c4
endlessorhub <anthonyhutchi99@gmail.com>
2022-05-10 17:00:04 -0400

    Add close button

--- apps/userve/src/components/layout/header.tsx
+++ apps/userve/src/components/layout/header.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { StaticImage } from "gatsby-plugin-image";
 
 import { Link } from "src/components/core";
-import { Header5 } from "src/components/core/typography";
 
 import { useAppDispatch, useAppSelector, AppActions } from 'src/store';
 const { toggleNavbarOpen } = AppActions;
@@ -59,11 +58,10 @@ const Header = () => {
 
 const HamburgerMenu = () => {
     const dispatch = useAppDispatch();
+    const isMobileMenuOpen = useAppSelector(state => state.navbar.isMobileMenuOpen);
     // return <button className="inline-flex items-center justify-center p2 rounded-md" aria-controls="mobile-menu" aria-expanded="false" onClick={() => dispatch(toggleNavbarOpen())} data-bs-toggle="collapse" data-bs-target={`#${MOBILE_DROPDOWN_ID}`}>
     return <button className="inline-flex items-center justify-center p2 rounded-md" aria-controls="mobile-menu" aria-expanded="false" onClick={() => dispatch(toggleNavbarOpen())}>
-        <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
+        {isMobileMenuOpen ? <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#000b8f" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/></svg> : <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#000b8f" aria-hidden="true"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" /></svg>}
     </button>
 };
 
