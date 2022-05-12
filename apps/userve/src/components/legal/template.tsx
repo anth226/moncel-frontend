d2e7a15ef8016908f1a724bc67866be5d343f9ef
endlessorhub <anthonyhutchi99@gmail.com>
2022-05-12 19:01:16 -0400

    Add SEO to legal

--- apps/userve/src/components/legal/template.tsx
+++ apps/userve/src/components/legal/template.tsx
@@ -4,38 +4,42 @@ import { graphql, PageProps } from "gatsby";
 import Head from 'src/components/head';
 import Layout from 'src/components/layout';
 import { DataProps } from 'src/lib/storyblokSourceTypes';
-import { LegalPageStoryblok } from "src/storyblok-component-types";
+import { LegalPageStoryblok, SeoStoryblok } from "src/storyblok-component-types";
 import { TextSection } from 'src/components/legal';
 
 const pageStyles = {
     fontFamily: "Inter,sans-serif",
 }
 
-interface PageContext {
-  content: LegalPageStoryblok,
-};
+export default ({ pageContext }: PageProps<DataProps, LegalPageStoryblok, SeoStoryblok>) => {
+ 
+  let seoContent; 
 
-export default ({ data, pageContext }: PageProps<DataProps, PageContext>) => {
-    const seoContent = data.seo.nodes[0];
+  if (pageContext.content.SEO) {
+    seoContent = pageContext.content.SEO[0] || '';
+  } else {
+    seoContent = '';
+  }
 
-    return <div>
-        <Head seo={seoContent}/>
-        <Layout>
-            <main style={pageStyles}>    
-              <TextSection {...pageContext.content} />
-            </main>
-        </Layout>
-    </div>
+  return <div>
+      <Head seo={seoContent}/>
+      <Layout>
+          <main style={pageStyles}>    
+            <TextSection {...pageContext.content} />
+          </main>
+      </Layout>
+  </div>
 
 }
-
 
 {/* const query = ``; // query default copy, then query copy for this url */}
 export const pageQuery = graphql`
   query {
     seo:allStoryblokEntry(filter: {full_slug: {eq: "seo"}}) {
       nodes {
-      full_slug
+        full_slug
+        slug 
+        content
       }
     }
 }`;
