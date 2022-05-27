5280e2f96723cdb81fedf7786cdf87cc24fe6d4d
mkoo-moncel <martin.koo@moncel.com>
2022-05-27 11:14:09 -0400

    switch to smartlook client lib and maybe fix cio

--- apps/userve/src/components/core/Analytics/smartlook.tsx
+++ apps/userve/src/components/core/Analytics/smartlook.tsx
@@ -1,16 +1,9 @@
-// @ts-nocheck
 import React, { useEffect } from 'react';
+import smartlookClient from 'smartlook-client'
 
 export default () => {
     useEffect(() => {
-        if(window) {
-            window.smartlook || (function(d) {
-                var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
-                var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
-                c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
-                })(document);
-                smartlook('init', '250d7a677249b463b4ed2568d28af20e159352a4');
-            }
+        smartlookClient.init('250d7a677249b463b4ed2568d28af20e159352a4')
     }, []);
     return null;
 };
