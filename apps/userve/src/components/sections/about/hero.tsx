077f5198594db3a9c756a7e59f281a20864f25a9
endlessorhub <anthonyhutchi99@gmail.com>
2022-07-15 13:55:24 -0400

    Fix css on about page

--- apps/userve/src/components/sections/about/hero.tsx
+++ apps/userve/src/components/sections/about/hero.tsx
@@ -10,10 +10,10 @@ import "./about.scss";
 
 export default (props: HeroStoryblok & StoryblokStoryProps) => {
     const fileNode = findMatchingLocalFileNode(getFilename(props.graphic?.filename || ""), props.story);
-    return <Section className="grid grid-cols-12 grid-rows-1 items-center">
+    return <Section className="grid grid-cols-12 grid-rows-1 items-center !pb-0">
         <div className="highlight-bold order-12 md:order-1 md:col-start-1 col-span-12 md:col-span-10">
             <Header1><ReactMarkdown>{props.title || ""}</ReactMarkdown></Header1>
-            <Header3><ReactMarkdown className="!mb-0">{props.description || ""}</ReactMarkdown></Header3>
+            <Header3 className="!mb-0"><ReactMarkdown className="!text-bluewood">{props.description || ""}</ReactMarkdown></Header3>
         </div>
         <div className="order-1 md:order-12 md:col-start-8 col-span-12 md:col-span-5 mb-6 md:mb-0 flex justify-center">
             <DynamicImage fileNode={fileNode} alt={props.title || ""} className="max-w-[300px] md:max-w-full"/>
