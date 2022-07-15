6f2eff89878825089ef682f4586ebc92b1737838
endlessorhub <anthonyhutchi99@gmail.com>
2022-07-15 14:11:27 -0400

    Fix css on about us page

--- apps/userve/src/components/sections/about/peopleTrust.tsx
+++ apps/userve/src/components/sections/about/peopleTrust.tsx
@@ -7,7 +7,7 @@ import { Header2, Text } from 'src/components/core/typography';
 import "./about.scss";
 
 export default ({ peopleData, trustData }: { peopleData: TitleDescriptionStoryblok, trustData: TitleDescriptionStoryblok }) => {
-    return <Section className="grid grid-cols-12 grid-rows-1 items-center !pb-0 pt-0 md:pt-6">
+    return <Section className="grid grid-cols-12 grid-rows-1 items-start !pb-0 pt-0 md:pt-20">
         <div className="highlight-bold order-12 md:order-1 col-span-12 md:col-span-5 pb-12 md:pb-0">
             <Header2>{peopleData.title || ""}</Header2>
             <Text className="!mb-0">{peopleData.description || ""}</Text>
