a5d3d2d124a324fbdce33b990cefdf1ddcfb9696
endlessorhub <anthonyhutchi99@gmail.com>
2022-07-18 11:36:45 -0400

    Delete smartlook.tsx

--- apps/userve/src/components/core/Analytics/smartlook.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-import smartlookClient from 'smartlook-client'
-
-export default () => {
-    useEffect(() => {
-        smartlookClient.init('250d7a677249b463b4ed2568d28af20e159352a4')
-    }, []);
-    return null;
-};
