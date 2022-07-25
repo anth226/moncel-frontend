3c71019491b042fae5c7ef64c903c931ecc62ca9
endlessorhub <anthonyhutchi99@gmail.com>
2022-07-25 11:40:38 -0400

    Update padding in two col check lists

--- apps/userve/src/components/shared/feature_box.tsx
+++ apps/userve/src/components/shared/feature_box.tsx
@@ -7,7 +7,7 @@ import { FeatureBoxStoryblok, ListStoryblok } from 'src/storyblok-component-type
 import { Header2, Header5 } from 'src/components/core/typography';
 
 const List = (list: ListStoryblok) => {
-    return <div className="flex flex-col col-span-12 md:col-span-6 mb-6 last:mb-0">
+    return <div className="flex flex-col col-span-12 md:col-span-6 mb-6 last:mb-0 relative">
         <img src={list.image?.filename} className="block max-w-none h-16 mb-4" height="64" width="64" alt={list.title} />
         {list.title == '' ? '' : <Header5>{list.title}</Header5>}
         <div className="checklist">
