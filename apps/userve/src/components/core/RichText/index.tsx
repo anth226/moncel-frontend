9adfe1c109547d7b14d49a771b72a0eb02e61d24
endlessorhub <anthonyhutchi99@gmail.com>
2022-08-24 09:37:40 -0400

    Update index.tsx

--- apps/userve/src/components/core/RichText/index.tsx
+++ apps/userve/src/components/core/RichText/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render } from 'storyblok-rich-text-react-renderer-ts';
+import { render, StoryblokRichtext } from 'storyblok-rich-text-react-renderer-ts';
 
 interface RichTextProps {
-    document: any;
+    document: StoryblokRichtext;
     className?: string;
 }
 
