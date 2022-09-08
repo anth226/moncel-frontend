38d701f5a0f74918c5c1bd80051aa63bca30e233
endlessorhub <anthonyhutchi99@gmail.com>
2022-09-08 09:18:31 -0400

    Update RichText to replace state placeholders

--- apps/userve/src/components/core/RichText/index.tsx
+++ apps/userve/src/components/core/RichText/index.tsx
@@ -4,12 +4,29 @@ import { render, StoryblokRichtext } from 'storyblok-rich-text-react-renderer-ts
 interface RichTextProps {
     document: StoryblokRichtext;
     className?: string;
+    state?: string;
 }
 
-function RichText({ document, className }:RichTextProps) {
+function RichText({ document, className, state }:RichTextProps) {
     // document is the rich text object you receive from Storyblok,
     // in the form { type: "doc", content: [ ... ] }
-    return <div className={className}>{render(document)}</div>;
+    // return <div className={className}>{render(document)}</div>;
+    
+    let newState : string;
+    if (state) {
+        newState = state;
+    } else {
+        newState = "";
+    }
+
+    return <div className={className}>{render(document, {
+        // replace $STATE placeholder in RichText component with a defined state
+        defaultStringResolver: (str) => (<p>{(str).replaceAll("$STATE", newState)}</p>),
+        markResolvers: {
+            code: (children) => <span>{children.toString().replaceAll("$STATE", newState) || ""}</span>
+        }
+    })}</div>;
+    
 }
 
 export default RichText;
