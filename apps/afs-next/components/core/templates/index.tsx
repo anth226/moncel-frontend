b1e1d12ae0c254534db50d5dfb2318ec9c22cb59
endlessorhub <anthonyhutchi99@gmail.com>
2022-11-02 11:18:10 -0400

    Update index.tsx

--- apps/afs-next/components/core/templates/index.tsx
+++ apps/afs-next/components/core/templates/index.tsx
@@ -84,7 +84,7 @@ export const ExpandIcon = () => <span className="text-teal">+</span>;
 export const CollapsibleTableCell = (props: CollapsibleTableCellProps) => {
 
   return <Accordion square classes={{ root: `!shadow-none !border-teal !border-t !border-l !border-r ${props.last ? '!border-b' : ''}` }} disableGutters {...props.accordionProps} expanded={props.expanded}>
-    <AccordionSummary className="!bg-haze w-full text-sm font-medium !px-3 !py-2 !border-b !border-teal !border-solid -mb-[1px]" classes={{ root: '!py-0 !px-3 min-h-0 !border-b !border-teal -mb-[1px]', content: ''}} expandIcon={<ExpandIcon/>} onClick={props.onClick}>
+    <AccordionSummary className="!bg-haze w-full text-sm font-medium !px-3 !py-2 !border-b !border-teal !border-solid !-mb-[1px]" classes={{ root: '!py-0 !px-3 min-h-0 !border-b !border-teal !-mb-[1px]', content: ''}} expandIcon={<ExpandIcon/>} onClick={props.onClick}>
       { props.summary }
     </AccordionSummary>
     <AccordionDetails classes={props.detailsClasses} >
