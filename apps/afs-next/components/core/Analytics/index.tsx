afa932aacb6760d494497cb36991093118aea930
Martin <martin.koo@moncel.com>
2022-11-04 10:45:41 -0400

    add newrelic nextjs monitor

--- apps/afs-next/components/core/Analytics/index.tsx
+++ apps/afs-next/components/core/Analytics/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import GTM from './gtm';
 import Mixpanel from './mixpanel';
 import CustomerIO from './customerio';
+import NewRelic from './newrelic';
 
 export default () => {
     return <>
         <GTM />
         <Mixpanel />
         <CustomerIO />
+        <NewRelic />
     </>
 };
