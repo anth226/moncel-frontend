a2fa1dae687b3990c787ceecbcc52c15918e0b6e
endlessorhub <anthonyhutchi99@gmail.com>
2022-11-07 09:21:46 -0500

    Add canonical link

--- apps/afs-next/components/core/layout/head.tsx
+++ apps/afs-next/components/core/layout/head.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router'
 
 const DEFAULT_TITLE = "Food Safety Courses | Online Food Safety Training and More";
 const DEFAULT_DESC = "AIFS is Australia's leading provider of Food Safety Supervisor and Food Handler training. Courses are Nationally Recognised and accepted by local health authorities.";
@@ -9,9 +10,11 @@ interface SeoOverrides {
 }
 
 const _Head = (props: SeoOverrides) => {
+    const router = useRouter()
     return <Head>
         <title>{props.title || DEFAULT_TITLE}</title>
         <meta name="description" content={props.description || DEFAULT_DESC} />
+        <link rel="canonical" href={`https://www.foodsafety.com.au${router.pathname}`} />
         <link rel="icon" href="/favicon.png" />
         {/* DO NOT put tracking here: https://nextjs.org/docs/advanced-features/custom-document */}
     </Head>;
