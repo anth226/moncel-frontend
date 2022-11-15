5c0019e8f0fc467ac137741dbd33f7a8b96c5432
endlessorhub <anthonyhutchi99@gmail.com>
2022-11-15 10:32:31 -0500

    Fix breadcrumbs on tablet

--- apps/afs-next/components/templates/index.tsx
+++ apps/afs-next/components/templates/index.tsx
@@ -20,10 +20,10 @@ import Button from 'components/core/Button';
 import { Courses } from 'data/courses';
 
 export const PathnameComponent = (props: { displayPathname: string, pathname: string }) => {
-    return <div className="hidden md:flex flex-row mb-4">
+    return <div className="hidden md:inline-block lg:flex mb-4 w-full">
         {props.displayPathname.split('/').map((e, i) => {
             if (!e) return null;
-            return <div className={`${(i > 0) ? styles['path-trailing-element'] : ''}`} key={`display-pathname-${i}`}><NextLink href={`/${props.pathname.split('/').slice(0, i + 1).join('/')}`}><a className={`!text-silver no-underline hover:underline text-sm tracking-wider font-medium`}>{props.displayPathname.split("/")[i].toUpperCase()}</a></NextLink></div>;
+            return <span className={`${(i > 0) ? styles['path-trailing-element'] : ''}`} key={`display-pathname-${i}`}><NextLink href={`/${props.pathname.split('/').slice(0, i + 1).join('/')}`}><a className={`!text-silver no-underline hover:underline text-sm tracking-wider font-medium`}>{props.displayPathname.split("/")[i].toUpperCase()}</a></NextLink></span>;
         })}
     </div>;
 };
