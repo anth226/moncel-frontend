09f7fae42865513dd07be41a0f4bf81d23369758
Martin <martin.koo@moncel.com>
2022-11-26 15:00:02 -0500

    deps

---
+++ apps/cfs-next/components/core/CollapsibleTable/index.tsx
@@ -0,0 +1,69 @@
+import { Children, ReactNode, cloneElement, isValidElement, useState } from 'react';
+import Accordion, { AccordionProps } from '@mui/material/Accordion';
+import AccordionDetails, { AccordionDetailsClasses } from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+
+interface CollapsibleTableCellProps {
+    summary: ReactNode;
+    details: ReactNode;
+    detailsClasses?: Partial<AccordionDetailsClasses>;
+    last?: boolean; // TODO: this can be used to prevent double borders
+    accordionProps?: Omit<AccordionProps, "children">;
+    expanded?: boolean;
+    onClick?: () => unknown;
+}
+
+export const CollapsibleTableCell = (props: CollapsibleTableCellProps) => {
+    const summaryBgClass = props.expanded ? "!bg-bunting" : "!bg-catskill";
+    const textClass = props.expanded ? "text-white" : "text-bunting";
+    return <Accordion square classes={{ root: `!shadow-none !border-teal !border-t !border-l !border-r ${props.last ? '!border-b' : ''}` }} disableGutters {...props.accordionProps} expanded={props.expanded}>
+      <AccordionSummary className={`${summaryBgClass} ${textClass} w-full text-sm font-medium !px-3 !py-2 !-mb-[1px]`} classes={{ root: '!py-0 !px-3 min-h-0 !-mb-[1px]', content: ''}} expandIcon={<ExpandIcon/>} onClick={props.onClick}>
+        { props.summary }
+      </AccordionSummary>
+      <AccordionDetails classes={props.detailsClasses} >
+          { props.details }
+      </AccordionDetails>
+    </Accordion>
+  };
+  
+  interface CollapsibleCellGroupProps {
+    children: ReactNode;
+    initialIndex?: number
+  }
+  interface CollapsibleCellGroupChild {
+    expanded: boolean | undefined;
+    onClick: () => unknown;
+    last: boolean;
+  }
+  /**
+   * HOC for creating a group of CollapsibleTableCells where only one can be expanded at a time
+   */
+  export const CollapsibleCellGroup = ({ children, initialIndex }: CollapsibleCellGroupProps) => {
+    const [ expandedIndex, setExpandedIndex ] = useState<number | undefined>(initialIndex);
+  
+    const handleCellClick = (n: number) => () => {
+      if(expandedIndex === n) setExpandedIndex(undefined);
+      else {
+        setExpandedIndex(n);
+      }
+    }
+    const newChildren = Children.map(children, (childElement, i) => {
+      if(!isValidElement<CollapsibleCellGroupChild>(childElement)) return childElement;
+      let last = false;
+      const childCount = Children.toArray(children).length;
+      // add border to last element
+      if(i == childCount - 1) {
+        last = true;
+      }
+      return cloneElement(childElement, {
+        expanded: expandedIndex === i,
+        onClick: handleCellClick(i),
+        last,
+      });
+    });
+  
+    return <>{ newChildren }</>;
+  
+}
+
+export const ExpandIcon = () => <span className="text-black">+</span>;
--- apps/cfs-next/pages/courses/food-handler-certification-course.tsx
+++ apps/cfs-next/pages/courses/food-handler-certification-course.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ReactNode } from 'react';
+import NextLink from 'next/link';
 import Layout from 'components/core/Layout';
 import { CoursesBackground, RowsTemplate } from 'components/Templates';
-import { Text, Header1, Header3, Header4 } from 'components/core/Typography';
+import { Text, Header1, Header3, Header4, P } from 'components/core/Typography';
 import Image from 'components/core/Image';
 import { Courses } from 'data/courses';
 import { NavigationProps, RowsTemplateProps } from 'components/Templates/types';
@@ -10,6 +11,7 @@ import useIsOnScreen from 'lib/useIsOnScreen';
 import HeroGraphic from 'public/courses/cifs-course.jpg';
 import IconCard from 'components/core/IconCard';
 import { LogosCarousel } from 'components/core/Carousel';
+import { CollapsibleTableCell, CollapsibleCellGroup } from 'components/core/CollapsibleTable';
 
 const ANCHOR_IDS = {
     intro: "intro",
@@ -167,8 +169,106 @@ const CourseOutlineSection = () => {
             <Header3 className="text-monza">Course outline</Header3>
             <Header4 className="text-bunting">Everything you need to become a Certified Food Handler, all in one course.</Header4>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-7">
+            <div className="flex flex-col text-bunting gap-4">
+                <p>The Food Handler Certification Course is divided into four easy to follow modules.</p>
+                <p>{`Each module has a series of lessons. Throughout the course, we keep you engaged and help you retain what you’ve learned with: `}</p>
+                <ul className="checklist">
+                    <li>useful study notes and fact sheets </li>
+                    <li>interactive learning activities </li>
+                    <li>interesting videos</li>
+                </ul>
+                <p>Our software features user-friendly navigation, requiring minimal training and quick onboarding.</p>
+                <p>Click on the 'Play' button below to watch a demo of the Food Handler Certification course.</p>
+                <div dangerouslySetInnerHTML={{ __html: `<div class="wistia_responsive_padding" style="padding:75.0% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><iframe src="https://fast.wistia.net/embed/iframe/jxt4z03owg?seo=false&videoFoam=true" title="CFS_Course_Demo_CoursePage Video" allow="autoplay; fullscreen" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" msallowfullscreen width="100%" height="100%"></iframe></div></div>
+                <script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>`}} />
+            </div>
+            <div className="flex flex-col">
+                <CollapsibleCellGroup initialIndex={0}>
+                    <CollapsibleTableCell
+                        summary={<p className="uppercase">Course Outline</p>}
+                        details={<>
+                            <div className="font-semibold">
+                                <p>Module one:</p>
+                                <p>Understanding food safety</p>
+                            </div>
+                            <p>Lesson 1 - Food Safety Laws {`&`} Responsibilities</p>
+                            <p>Lesson 2 - Food Contamination {`&`} High-Risk Groups</p>
+                            <p>Lesson 3 - Types of Contamination</p>
 
+                            <div className="font-semibold mt-4">
+                                <p>Module two:</p>
+                                <p>Preventing food-borne illness</p>
+                            </div>
+                            <p>Lesson 4 - Personal hygeine {`&`} workspace behaviour</p>
+                            <p>Lesson 5 - Time {`&`} temperature control</p>
+                            <p>Lesson 6 - Cleaning {`&`} sanitizing</p>
+                            <p>Lesson 7 - Maintaining food premises {`&`} equipment</p>
+
+                            <div className="font-semibold mt-4">
+                                <p>Module three:</p>
+                                <p>Working with food safely</p>
+                            </div>
+                            <p>Lesson 8 - Purchasing, receiving {`&`} storing food safely</p>
+                            <p>Lesson 9 - Preparing food safely</p>
+                            <p>Lesson 10 - Serving {`&`} dispensing food safely</p>
+
+                            <div className="font-semibold mt-4">
+                                <p>Module Four</p>
+                                <p>Managing your food safety plan</p>
+                            </div>
+                            <p>Lesson 11 - Managing your food safety plan</p>
+
+                        </>}
+                        detailsClasses={{ root: '!p-4 bg-lilac' }} />
+                    <CollapsibleTableCell
+                        summary={<p className="uppercase">Certification Process</p>}
+                        details={
+                            <>
+                                <Text>{`To successfully obtain your CIFS Food Handler Certification, you’re required to complete each lesson and pass a final exam within 30 days.`}</Text>
+                                <Text>{`The final exam has 50 multiple choice questions and a 60 minute time limit. A minimum score of 70% is required to pass the exam. You have unlimited attempts to write the final exam during your 30 day course access period.`}</Text>
+                                <Text>{`By law, the test requires proctoring. A Proctor is a person who remotely observes you via a webcam while you complete the exam. Proctors are available 24 hours a day, 7 days a week, allowing you to complete the exam at any time. Simply choose a time that suits you and get started.`}</Text>
+                            </>
+                        }
+                        detailsClasses={{ root: '!p-4 bg-lilac' }} />
+
+                    <CollapsibleTableCell
+                        summary={<p className="uppercase">Technical Requirements</p>}
+                        details={
+                            <>
+                                <Text>{`The course can be completed on your mobile phone, tablet or computer. You will need to have a modern web browser and it’s important that you have Javascript enabled. The final exam (which takes only one hour) requires a computer with a webcam, microphone and a reliable internet connection. `}</Text>
+                                <Text>In order to access course resources, some students are prompted to download Adobe PDF Reader or Adobe Acrobat (both programs are free). We recommend <a href="font-semibold" target="_blank">downloading the latest version of Adobe PDF Reader.</a></Text>
+                                <Text><NextLink href="/contact"><a className="font-semibold">Contact us</a></NextLink> {`if you have any concerns about the technical requirements for the final exam. Our support team can help troubleshoot or even help you find a computer with a webcam to ensure taking your final exam is a smooth process.`}</Text>
+                            </>
+                        }
+                        detailsClasses={{ root: '!p-4 bg-lilac' }} />
+
+                    <CollapsibleTableCell
+                        summary={<p className="uppercase">Provinces and Territories</p>}
+                        details={
+                            <>
+                                <Text>{`The CIFS Food Handler Certification course is approved/accepted in every province and territory across Canada:`}</Text>
+                                <ul className="font-semibold">
+                                    <li><NextLink href="/laws-requirements/by-location/alberta"><a>Alberta</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/british-columbia"><a>British Columbia</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/manitoba"><a>Manitoba</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/new-brunswick"><a>New Brunswick</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/newfoundland"><a>Newfoundland and Labrador</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/nwt"><a>Northwest Territories</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/nova-scotia"><a>Nova Scotia</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/nunavut"><a>Nunavut</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/ontario"><a>Ontario</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/pei"><a>Prince Edward Island</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/quebec"><a>Quebec</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/saskatchewan"><a>Saskatchewan</a></NextLink> </li>
+                                    <li><NextLink href="/laws-requirements/by-location/yukon"><a>Yukon</a></NextLink> </li>
+                                </ul>
+                            </>
+                        }
+                        detailsClasses={{ root: '!p-4 bg-lilac' }} />
+                </CollapsibleCellGroup>
+
+            </div>
         </div>
 
     </div>;
@@ -187,5 +287,6 @@ const CommonQuestionsSection = () => {
 };
 
 const Divider = () => <div className="w-full col-span-3 border-t-[1px] border-catskill" />
+export const ListItem = ({ children }: { children?: ReactNode }) => <li className="mb-2 last:mb-0">{ children }</li>;
 
 export default Page;
